0cf81a0f434de28d0b3b8ee7cd81bedfbd8afd3e
Ramona M <r1mari@yahoo.com>
2018-06-27 08:53:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(response)
   #text=" ".upcase
   if response=='I LOVE YOU GRANDMA!'
-     puts'I LOVE YOU TOO PUMPKIN!'
+     return'I LOVE YOU TOO PUMPKIN!'
      
   elsif response== response.upcase
      puts = "NO, NOT SINCE 1938!" 
